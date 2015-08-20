5f576f8ea238184f814854e8686c1eb5f5c4cfcd
Taras Kyryliuk <tkiriluk@gmail.com>
2015-08-21 00:07:28 +0300

    Update delete_old.sh to run on all folders in backups/

--- delete_old.sh
+++ delete_old.sh
@@ -6,25 +6,22 @@ CURRENT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #include file with settings
 . "$CURRENT_PATH"/settings
 
-while
-    read -a STRING_ARRAY;
-do
-    PROJECT_NAME=${STRING_ARRAY[0]};
+for d in $CURRENT_PATH/backups/*/ ; do
     
     #clean 01_Yearly directory
-    cd "$CURRENT_PATH"/backups/"$PROJECT_NAME"/01_Yearly/
+    cd $d/01_Yearly/
     (ls -t|head -n "$YEARLY";ls)|sort|uniq -u|xargs --no-run-if-empty rm -rf
     
     #clean 02_Monthly directory
-    cd "$CURRENT_PATH"/backups/"$PROJECT_NAME"/02_Monthly/
+    cd $d/02_Monthly/
     (ls -t|head -n "$MONTHLY";ls)|sort|uniq -u|xargs --no-run-if-empty rm -rf
     
     #clean 03_Weekly directory
-    cd "$CURRENT_PATH"/backups/"$PROJECT_NAME"/03_Weekly/
+    cd $d/03_Weekly/
     (ls -t|head -n "$WEEKLY";ls)|sort|uniq -u|xargs --no-run-if-empty rm -rf
     
     #clean 04_Daily directory
-    cd "$CURRENT_PATH"/backups/"$PROJECT_NAME"/04_Daily/
+    cd $d/04_Daily/
     (ls -t|head -n "$DAILY";ls)|sort|uniq -u|xargs --no-run-if-empty rm -rf
     
-done < "$CURRENT_PATH"/projects_list
+done
